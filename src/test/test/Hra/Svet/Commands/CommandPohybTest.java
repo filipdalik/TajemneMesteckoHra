afe8108947dc0d0c216d1a64c5131988078380ed
Filip Dalík <email@email.com>
2026-02-06 10:57:39 +0100

    vytvoreni CommandPohybTest

---
+++ src/test/test/Hra/Svet/Commands/CommandPohybTest.java
@@ -0,0 +1,31 @@
+package Hra.Svet.Commands;
+
+import Hra.Svet.Hrac;
+import Hra.Svet.Lokace;
+import Hra.Svet.Predmet;
+import Hra.Svet.SpravovaniLokaci;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandPohybTest {
+
+    SpravovaniLokaci spravovaniLokaci;
+    Hrac hrac;
+    CommandPohyb commandPohyb;
+    Lokace lokace1;
+    Lokace lokace2;
+
+
+
+    @BeforeEach
+    void setUp(){
+
+    }
+    @org.junit.jupiter.api.Test
+    void execute() {
+
+    }
+}
