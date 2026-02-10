038573954cf30739fe2bd12a0f3999949f54d73e
Filip <dalik2@spsejecna.cz>
2026-02-10 16:05:21 +0100

    Test commandu pro pohyb

--- src/test/test/Hra/Svet/Commands/CommandPohybTest.java
+++ src/test/test/Hra/Svet/Commands/CommandPohybTest.java
@@ -1,31 +1,52 @@
-package Hra.Svet.Commands;
-
-import Hra.Svet.Hrac;
-import Hra.Svet.Lokace;
-import Hra.Svet.Predmet;
-import Hra.Svet.SpravovaniLokaci;
+package test.test.Hra.Svet.Commands;
+import Hra.Svet.*;
+import Hra.Svet.Commands.CommandPohyb;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CommandPohybTest {
 
-    SpravovaniLokaci spravovaniLokaci;
-    Hrac hrac;
-    CommandPohyb commandPohyb;
-    Lokace lokace1;
-    Lokace lokace2;
+        CommandPohyb commandPohyb;
+        Hrac hrac;
+        SpravovaniLokaci spravovaniLokaci;
 
+        @BeforeEach
+        void setUp() {
+            spravovaniLokaci = new SpravovaniLokaci();
+            ArrayList<Lokace> seznamLokaci = new ArrayList<>();
 
+            Lokace start = new Lokace();
+            start.setId("start_id");
+            start.setJmeno("Start");
+            start.setZamceno(false);
 
-    @BeforeEach
-    void setUp(){
+            Lokace cil = new Lokace();
+            cil.setId("cil_id");
+            cil.setJmeno("Cil");
+            cil.setZamceno(false);
 
+            Pripojeni pripojeni = new Pripojeni();
+            pripojeni.setRovne("cil_id");
+
+            start.setPripojeni(pripojeni);
+            seznamLokaci.add(start);
+            seznamLokaci.add(cil);
+
+            spravovaniLokaci.setLokace(seznamLokaci);
+            hrac = new Hrac("TestHrac", "start_id", spravovaniLokaci);
+            hrac.setiDMistnosti("start_id");
+            commandPohyb = new CommandPohyb(hrac, spravovaniLokaci);
+        }
+
+        @Test
+        void execute() {
+
+            String[] parametry = {null, "rovne"};
+            commandPohyb.execute(parametry);
+            assertEquals("cil_id", hrac.getiDMistnosti(), "Hrac se mel presunout do cilove lokace.");
+        }
     }
-    @org.junit.jupiter.api.Test
-    void execute() {
-
-    }
-}
